307c46e6f2fe34dbf1870bd34a01f1325b22ac8b
Sebas-crypt <usefullseba17@gmail.com>
2024-03-28 00:10:22 -0400

    fixing minor code errors

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE IF NOT EXIST hbnb_test_db;
-CREATE USER IF NOT EXIST 'hbnb_test'@'localhost'
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost'
 IDENTIFIED BY 'hbnb_test_pwd'
-GRANT ALL PRIVILEGES ON *hbnb_test_db* TO 'hbnb_test'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *hbnb_test_db* TO 'hbnb_test'@'localhost';
 GRANT PRIVILEGES SELECT ON *performance_schema* TO 'hbnb_test'@'localhost';
