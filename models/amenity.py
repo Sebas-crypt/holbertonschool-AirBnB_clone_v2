4804d264b1955dfd42bd97b03c20e8fc4cfa7eff
MorrisCastro <jomarcastro2004@gmail.com>
2024-03-29 15:57:30 -0400

    change amenity class to db class

--- models/amenity.py
+++ models/amenity.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey, Table
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.place import place_amenity
-
+from os import getenv
 
 
 class Amenity(BaseModel, Base):
-    """ class Amenity inherits from BaseModel and Base """
-    __tablename__ = "amenities"
+    """
+    Amenities that a place might have
+    """
+    __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity)
-    # name = ""
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        place_amenities = relationship("Place", secondary='place_amenity',
+                                       back_populates='amenities')
