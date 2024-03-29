60521024bf5c26c8d9f37931dfd530ffd4799fd4
MorrisCastro <jomarcastro2004@gmail.com>
2024-03-29 15:55:31 -0400

    remake user

--- models/user.py
+++ models/user.py
@@ -1,17 +1,25 @@
+
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = "users"
-    email = Column('email', String(128), nullable=False)
-    password = Column('password', String(128), nullable=False)
-    first_name = Column('first_name', String(128), nullable=True, default="NULL")
-<<<<<<< HEAD
-    last_name = Column('last_name', String(128), nullable=True, defaulr="NULL")
-=======
-    last_name = Column('last_name', String(128), nullable=True, default="NULL")
->>>>>>> seba
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+
+    def __init__(self, *args, **kwargs):
+        """ inherit from base and Basemodel """
+        super().__init__(*args, **kwargs)
+
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
