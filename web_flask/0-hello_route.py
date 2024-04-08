eff72fbb96fff9483f67842413ade61d3fea3fcb
Sebas-crypt <usefullseba17@gmail.com>
2024-04-08 10:31:57 -0400

    modifying task 0 a bit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__main__)
 
-@app.route("/")
+@app.route("/, strict_slashes = false")
+
 def hello():
     return 'Hello HBNB!'
+
+if name = __main__:
+    app.run ('host=0.0.0.0', 'port=5000')
