b2a161abf1f8f7022336ae6d73606df9ed15208d
Sebas-crypt <usefullseba17@gmail.com>
2024-04-12 10:23:47 -0400

    running checkers

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 @app.route("/", strict_slashes =False)
 
 def hello():
-    return ('Hello HBNB!')
+    """ Returns some text. """
+    return 'Hello HBNB!'
 
 if __name__ == '__main__':
-    app.run (host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
