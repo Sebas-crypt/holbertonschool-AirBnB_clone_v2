09cbfbc7b9bcbfa5b9d26a01f5bcdb4945e54074
Sebas-crypt <usefullseba17@gmail.com>
2024-04-17 11:04:16 -0400

    changing come coms

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -2,12 +2,12 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello():
     """ Returns some text. """
     return 'Hello HBNB!'
 
-@app.route("/HBNB", strict_slashes=False)
+@app.route('/HBNB', strict_slashes=False)
 def hbnb():
     """ Return other text. """
     return 'HBNB'
