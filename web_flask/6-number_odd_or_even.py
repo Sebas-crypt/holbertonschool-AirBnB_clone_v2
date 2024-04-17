04ef60025ee38002f488a9d9d1524cf2af982413
Sebas-crypt <usefullseba17@gmail.com>
2024-04-17 14:24:24 -0400

    fixing comments

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" starts the flask app """
+"""Starts the flask app."""
 from flask import Flask
 from flask import render_template
 
@@ -8,45 +8,45 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
-    """ Returns some text. """
+    """Return hello HBNB."""
     return 'Hello HBNB!'
 
 
-@app.route('/HBNB', strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ Returns other text. """
+    """Return HBNB."""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def text(text):
-    """ Replace text with variable. """
+    """Return C."""
     return 'C ' + text.replace('_', ' ')
 
 
 @app.route('/python/', defaults={'text': "is cool"}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def texts(text):
-    """ Replace more text with another variable. """
+    """Return python."""
     return 'Python ' + text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def numbers(n):
-    """ Replace with int only if given int. """
+def number(n):
+    """Return the number."""
     return str(n) + " is a number"
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_templates(n):
-    """ Display html if n is int. """
-        return render_template ("5-number.html", n=n)
+    """Display html only if n is an integer."""
+    return render_template("5-number.html", n=n)
 
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def odd_or_even(n):
-    """ Display different page depending on var given odd or even. """
-    return render_template ("6-number_odd_or_even.html", n=n)
+    """Return odd or even a number."""
+    return render_template("6-number_odd_or_even.html", n=n)
 
 
 if __name__ == '__main__':
