7522debaae1cdb06a8a132187dc3d98f47ede078
Sebas-crypt <usefullseba17@gmail.com>
2024-04-17 13:54:06 -0400

    organizing the code in 2 so it passes checkers

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -6,21 +6,24 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes =False)
+@app.route('/', strict_slashes=False)
 def hello():
     """ Returns some text. """
     return 'Hello HBNB!'
 
-@app.route('/HBNB', strict_slashes =False)
+
+@app.route('/HBNB', strict_slashes=False)
 def hbnb() :
     """ Return other text. """
     return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def text(text):
     """ Replace text with a variable. """
     text = text.replace ("_", " ")
     return("C {}".format(text))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
